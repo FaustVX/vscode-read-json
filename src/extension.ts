4896e8cd61389f4d3043a8beaf97cad62f260c72
FaustVX <jodu035@gmail.com>
2023-11-12 22:55:49 +0100

    Added complete feature

--- src/extension.ts
+++ src/extension.ts
@@ -1,23 +1,44 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import * as json5 from 'json5';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
+interface Args {
+	filePath: string;
+	jsonPath: string | undefined;
+}
+
 export function activate(context: vscode.ExtensionContext) {
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "helloworld-sample" is now active!');
+	async function readJson(args: Args) : Promise<string | null> {
+		let obj = await readFile(args.filePath);
+		if (args.jsonPath !== undefined)
+			obj = jsonPath(obj, args.jsonPath);
+		if (typeof obj === "object")
+			return await showQuickPick(obj) as string;
+		return obj;
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
+		function jsonPath(obj: any, path: string) : any {
+			path.split('.').forEach(element => {
+				obj = obj[element];
+			});
+			return obj;
+		}
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World!');
-	});
+		async function readFile(path: string) : Promise<object> {
+			const uri = vscode.Uri.file((vscode.workspace.workspaceFolders as vscode.WorkspaceFolder[])[0].uri.path + `/${path}`);
+			const data = await vscode.workspace.fs.readFile(uri);
+			const file = Buffer.from(data).toString('utf8');
+			return json5.parse(file);
+		}
 
-	context.subscriptions.push(disposable);
+		async function showQuickPick(obj: any) : Promise<string | object | null> {
+			const result = await vscode.window.showQuickPick(Object.keys(obj));
+			if (result === undefined) {
+				return null;
+			} else if (typeof obj[result] === "object") {
+				return showQuickPick(obj[result]);
+			} else {
+				return obj[result];
+			}
+		}
+	}
+	context.subscriptions.push(vscode.commands.registerCommand('read-json.read', readJson));
 }
