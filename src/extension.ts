2a336ad6bbacf5b1f24dad4d9d889cad0344952e
FaustVX <jodu035@gmail.com>
2023-11-13 18:38:01 +0100

    Read JSON from explorer tab

--- src/extension.ts
+++ src/extension.ts
@@ -7,13 +7,43 @@ interface Args {
 }
 
 export function activate(context: vscode.ExtensionContext) {
-	async function readJson(args: Args) : Promise<string | null> {
+	console.log("read-json activated");
+	async function readJson(args: Args | vscode.Uri | undefined) : Promise<string | null> {
+		console.log("read-json.read ran");
+		args = await setArgs(args);
 		let obj = await readFile(args.filePath);
 		if (args.jsonPath !== undefined)
 			obj = jsonPath(obj, args.jsonPath);
 		if (typeof obj === "object")
 			return await showQuickPick(obj) as string;
 		return obj;
+
+		async function setArgs(args: Args | vscode.Uri | undefined) : Promise<Args> {
+			if (args instanceof vscode.Uri)
+				return {
+					filePath: args.path,
+					jsonPath: undefined,
+				};
+			else if (args === undefined) {
+				const file = await vscode.window.showOpenDialog({
+					title: "Open JSON file",
+					filters: {
+						"JSON": ["json"]
+					}
+				});
+				if (file)
+				return {
+					filePath: file[0].path,
+					jsonPath: undefined,
+				};
+			} else {
+				return {
+					filePath: (vscode.workspace.workspaceFolders as vscode.WorkspaceFolder[])[0].uri.path + `/${args.filePath}`,
+					jsonPath : args?.jsonPath
+				};
+			}
+			return {filePath: "", jsonPath: undefined};
+		}
 
 		function jsonPath(obj: any, path: string) : any {
 			path.split('.').forEach(element => {
@@ -23,7 +53,7 @@ export function activate(context: vscode.ExtensionContext) {
 		}
 
 		async function readFile(path: string) : Promise<object> {
-			const uri = vscode.Uri.file((vscode.workspace.workspaceFolders as vscode.WorkspaceFolder[])[0].uri.path + `/${path}`);
+			const uri = vscode.Uri.file(path);
 			const data = await vscode.workspace.fs.readFile(uri);
 			const file = Buffer.from(data).toString('utf8');
 			return json5.parse(file);
